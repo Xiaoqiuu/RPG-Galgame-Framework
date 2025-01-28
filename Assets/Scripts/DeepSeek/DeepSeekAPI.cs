a00be55b1d8d37766ca9274469f83c0527076a32
xiaoqiuu <xiaoqiuuu@gmail.com>
2025-01-29 01:01:01 +0800

    Scripts structure changed

---
+++ Assets/Scripts/DeepSeek/DeepSeekAPI.cs
@@ -0,0 +1,14 @@
+using System;
+using UnityEngine;
+
+namespace DeepSeek
+{
+    
+    public class DeepSeekAPI : MonoBehaviour
+    {
+        private void Start()
+        {
+            //throw new NotImplementedException();
+        }
+    }
+}
